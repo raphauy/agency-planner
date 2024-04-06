3c804a882a09d27328fc316de8e3e4a841a3008e
raphauy <rapha.uy@rapha.uy>
2024-04-06 12:33:32 -0300

    small improve

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -11,13 +11,13 @@ export default async function Logo() {
         user ? (
         <Link href="/">
           <div className="text-4xl font-bold flex items-center pb-1 pl-3">
-            <p className="text-gray-700">a</p>
+            <p className="text-gray-700 dark:text-white">a</p>
             <p className="text-gray-300">p</p>
           </div>
         </Link> ) : (
           <Link href="/">
           <div className="text-2xl font-bold pl-3 flex py-2">
-            <p className="text-gray-700 whitespace-nowrap">agency-planner</p>
+            <p className="text-gray-700 whitespace-nowrap dark:text-white">agency-planner</p>
             <p className="text-gray-300">.com</p>
           </div>
         </Link>
