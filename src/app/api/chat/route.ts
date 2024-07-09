8006e24d1a7169d4ba3e646e072439eb26d200eb
raphauy <rapha.uy@gmail.com>
2024-07-09 15:19:11 -0300

    copy lab last20

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -11,7 +11,7 @@ export const maxDuration = 299
 
 export async function POST(req: Request) {
 
-  const { messages, conversationId, clientSlug } = await req.json()
+  const { messages, conversationId, clientSlug, prompt } = await req.json()
 
   const currentUser= await getCurrentUser()
   if (!currentUser || !currentUser.email) return new Response("No se encontró un usuario logueado", { status: 404 })
@@ -46,8 +46,10 @@ export async function POST(req: Request) {
     return new Response("Client prompt not found", { status: 404 })
   }
 
+  // take the last 20 messages
+  const last20= messages.slice(-20)
 
-  const lastMessage= messages[messages.length - 1]
+  const lastMessage= last20[last20.length - 1]
   const input= lastMessage.content
   if (lastMessage.role === "user" && input) {
     console.log("input: " + input)
@@ -67,8 +69,8 @@ export async function POST(req: Request) {
   console.log("systemMessage", systemMessage)
   
 
-  console.log("messages.count: " + messages.length)
-  console.log("messages", JSON.stringify(messages, (key, value) => {
+  console.log("messages.count: " + last20.length)
+  console.log("messages", JSON.stringify(last20, (key, value) => {
     if (typeof value === 'object' && value !== null) {
       return value;
     }
@@ -78,7 +80,7 @@ export async function POST(req: Request) {
   const result= await streamText({
     model: openai("gpt-4o"),
     system: systemMessage,
-    messages: convertToCoreMessages(messages),
+    messages: convertToCoreMessages(last20),
     tools,
     onFinish: async ({text, toolCalls, toolResults, finishReason, usage,}) => {
       console.log("onFinish")
@@ -99,38 +101,11 @@ export async function POST(req: Request) {
       console.log("usage", usage)
       console.log("toolResults", toolResults)
       
-      // if (finishReason === "tool-calls" && toolCalls) {
-      //   // Handle tool calls
-      //   for (const toolCall of toolCalls) {
-      //     if (toolCall.toolName === "entregarCopys") {
-      //       const messageForm: MessageFormValues= {
-      //         role: "function",
-      //         content: JSON.stringify(toolCall.args),
-      //         name: toolCall.toolName,
-      //         tokens: usage.completionTokens + (usage.promptTokens * 3),
-      //         conversationId: conversation.id,
-      //       }
-      //       await createMessage(messageForm)
-      //     }
-      //   }
-      // }
       
       if (!toolResults) return
 
       // Handle tool results
       for (const toolResult of toolResults) {
-        // if (toolResult.toolName === "obtenerDocumento") {
-        //   const document: DocumentResult= toolResult.result as DocumentResult
-        //   const messageForm: MessageFormValues= {
-        //     role: "function",
-        //     content: document.documentName,
-        //     name: toolResult.toolName,
-        //     tokens: usage.completionTokens + (usage.promptTokens * 3),
-        //     toolInvocations: JSON.stringify(toolResult),
-        //     conversationId: conversation.id,
-        //   }
-        //   await createMessage(messageForm)
-        // }
         const messageForm: MessageFormValues= {
           role: "assistant",
           content: "",
@@ -141,105 +116,12 @@ export async function POST(req: Request) {
         }
         await createMessage(messageForm)
 
-      }
+      }      
 
-      
-
-      return;
+      return
     }
   })
 
-
-
   return result.toAIStreamResponse();
-
-
-  // functions.forEach((functionDefinition) => {
-  //   console.log("functionDefinition: " + functionDefinition.name);
-  // })
-
-  // const openai = new OpenAI({
-  //   apiKey: process.env.OPENAI_API_KEY!,
-  // })
-  
-  // Inicializa el objeto de argumentos con propiedades comunes
-  let baseArgs = {
-    model: "gpt-4o",
-    temperature: 0,
-    stream: true,
-  };
-  let promptTokens= 0
-  let completionTokens= 0
-
-  // @ts-ignore
-  baseArgs = { ...baseArgs, messages: messages }
-
-  // Si el array de functions tiene al menos un elemento, añade el parámetro functions
-//  const args = functions.length > 0 ? { ...baseArgs, functions: functions, function_call: "auto" } : baseArgs;
-  const args= baseArgs
-
-  // Ahora args contiene el parámetro functions solo si el array no estaba vacío
-  //const initialResponse = await openai.chat.completions.create(args as any);
-
-//   const stream = OpenAIStream(initialResponse, {
-//     experimental_onFunctionCall: async (
-//       { name, arguments: args,  },
-//       createFunctionCallMessages,
-//     ) => {
-// //      const result = await runFunction(name, args, clientId);
-//       const result = await processFunctionCall(clientId, name, args);
-//       const newMessages = createFunctionCallMessages(result);
-
-//       let baseArgs = {
-//         model: model.name,
-//         stream: true,
-//       };
-    
-//       // @ts-ignore
-//       baseArgs = { ...baseArgs, messages: [...messages, ...newMessages] };
-//       const recursiveArgs = functions.length > 0 ? { ...baseArgs, functions: functions, function_call: "auto" } : baseArgs;
-
-//       return openai.chat.completions.create(recursiveArgs as any);
-
-//     },
-//     onStart: async () => {
-//       console.log("start")
-//       const text= messages[messages.length - 1].content
-//       console.log("text: " + text)
-      
-//       const messageStored= await messageArrived(phone, text, client.id, "user", "")
-//       if (messageStored) console.log("user message stored")
-
-//     },
-//     onCompletion: async (completion) => {
-//       console.log("completion: ", completion)
-
-//       const partialPromptToken = openaiTokenCounter.chat(messages, "gpt-4") + 1
-//       console.log(`\tPartial prompt token count: ${partialPromptToken}`)      
-//       promptTokens += partialPromptToken
-
-//       const completionMessages = [
-//         { role: "assistant", content: completion },
-//       ]
-//       const partialCompletionTokens = openaiTokenCounter.chat(completionMessages, "gpt-4")
-//       console.log(`\tPartial completion token count: ${partialCompletionTokens}`)
-//       completionTokens += partialCompletionTokens
-
-//       if (!completion.includes("function_call")) {
-//         console.log(`Prompt token count: ${promptTokens}`)
-//         console.log(`Completion token count: ${completionTokens}`)
-//         const messageStored= await messageArrived(phone, completion, client.id, "assistant", "", promptTokens, completionTokens)
-//         if (messageStored) console.log("assistant message stored")
-//       } else {
-//         // console.log("function call")
-//         // const completionObj= JSON.parse(completion)
-//         // const { name, arguments: args }= completionObj.function_call
-// //        const text= `Llamando a la función ${name} con los argumentos: ${args}`
-//         // const text= `Función invocada.`
-//         // const messageStored= await messageArrived(phone, text, client.id, "function", "", 0, 0)
-//         // if (messageStored) console.log("function message stored")
-//       }
-//     },
-//   });
 
 }
