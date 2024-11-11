9cf90b9adb9e5a31bdaa9bfff1b3fc0abd75394a
raphauy <rapha.uy@rapha.uy>
2024-11-11 09:57:11 -0300

    layout center fix

--- src/app/[agencySlug]/[clientSlug]/instagram/layout.tsx
+++ src/app/[agencySlug]/[clientSlug]/instagram/layout.tsx
@@ -1,7 +1,7 @@
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="h-full w-full max-w-[1350px]">
+        <div className="h-full w-full max-w-[1350px] flex flex-col items-center">
             {children}
         </div>
     )
