bb95d84cf447bfcb6589da70e676673387dac327
raphauy <rapha.uy@rapha.uy>
2025-05-15 04:52:02 -0300

    file download maxDuration

--- src/app/api/download-zip/route.ts
+++ src/app/api/download-zip/route.ts
@@ -1,6 +1,8 @@
 import { type NextRequest } from "next/server"
 import AdmZip from "adm-zip"
 
+export const maxDuration = 299
+
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const urlsParam = searchParams.get('urls')
--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -1,5 +1,7 @@
 import { type NextRequest } from "next/server"
 
+export const maxDuration = 299
+
 export async function GET(request: NextRequest) {
 
     const searchParams = request.nextUrl.searchParams
